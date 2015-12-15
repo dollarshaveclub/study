ee4b189ad3699bfc8d310b5663a94059d0c842a3
Jacob Kelley <jakie8@gmail.com>
2015-12-15 10:44:05 -0800

    Bookmarklet

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,8 @@
 import babelify from 'babelify';
+import bookmarklet from 'bookmarklet';
 import browserify from 'browserify';
 import buffer from 'vinyl-buffer';
+import fs from 'fs';
 import gulp from 'gulp';
 import rename from 'gulp-rename';
 import source from 'vinyl-source-stream';
@@ -24,12 +26,28 @@ gulp.task('default', () => {
     .pipe(source('test-interface.min.js'))
     .pipe(buffer())
     .pipe(uglify())
+
+    .pipe(function () {
+      console.log(arguments);
+    })
+
     .pipe(gulp.dest('build'));
+
 });
 
 gulp.task('do-watch', () => {
   gulp.watch('lib/**/*.js', ['default']);
 });
 
+gulp.task("bookmarklet", cb => {
+  var data = fs.readFileSync('./build/test-interface.min.js', 'utf-8');
+  var code = bookmarklet.convert(data, {
+
+  });
+
+  var readme = fs.readFileSync('./assets/README.md', 'utf-8');
+  readme = readme.replace('_BOOKMARKLET_', code);
+  fs.writeFile('./README.md', readme, cb);
+});
 
 gulp.task('watch', ['default', 'do-watch']);
