3a8528e881422508ec84b91c2d81e326b1b4a4c8
Jacob Kelley <jakie8@gmail.com>
2016-11-14 12:15:02 -0800

    Cleaning up some logic

--- src/study.js
+++ src/study.js
@@ -6,7 +6,6 @@ const storageKey = 'ab-tests';
 const noop = () => {};
 const rand = (min, max) => Math.random() * ((max - min) + min);
 
-
 const chooseWeightedItem = (names, weights) => {
   // Total out the number of weights
   const total = weights.reduce((a, b) => a + b);
@@ -27,18 +26,27 @@ const chooseWeightedItem = (names, weights) => {
 };
 
 class Study {
-  constructor(name, defaultData = {}, { persist = true, active = true, chosen = noop } = {}) {
-    const data = defaultData;
-
+  constructor(name, data = {}, { persist = true, active = true, chosen = noop } = {}) {
     if (!name) { throw new Error('Tests must have a name'); }
 
-    // Return if inactive
-    if (!active) {
+    this.name = name;
+    this.data = data;
+    this.persist = persist;
+    this.active = active;
+    this.chosen = chosen;
+
+    if (!this.active) {
       return {
         active: false,
       };
     }
 
+    this.fetchTests();
+    this.assignBucket();
+    return this.dispatchTest();
+  }
+
+  fetchTests() {
     // Set test name
     let tests = storage.local.getItem(storageKey);
     if (tests) {
@@ -50,84 +58,87 @@ class Study {
     } else {
       tests = {};
     }
+    this.tests = tests;
+  }
 
-    let bucket;
-
+  assignBucket() {
     // Grab assignment via query param. Syntax:
     // ?assign=test:bucket
     const [assignTest, assignBucket] = (getParam('assign') || '').split(':');
-    const assignmentMatchesName = assignTest === name;
-    const isValidBucket = assignBucket in data;
+    const assignmentMatchesName = assignTest === this.name;
+    const isValidBucket = assignBucket in this.data;
 
-    const useStoredAssignment = persist && tests[name];
+    const useStoredAssignment = this.persist && this.tests[this.name];
     const shouldAssign = (assignmentMatchesName && isValidBucket) || !useStoredAssignment;
-
     // Retrieve Bucket from storage if possible
+
+
     if (!shouldAssign) {
-      bucket = tests[name].bucket;
+      this.bucket = this.tests[this.name].bucket;
     } else {
       // Determine Bucket
       // Get a list of test names and test weights
-      const names = Object.keys(data);
+      const names = Object.keys(this.data);
       const weights = [];
 
       names.forEach((bucketName) => {
-        if (typeof data[bucketName].weight === 'undefined') {
-          data[bucketName].weight = 1;
+        if (typeof this.data[bucketName].weight === 'undefined') {
+          this.data[bucketName].weight = 1;
         }
-        weights.push(data[bucketName].weight);
+        weights.push(this.data[bucketName].weight);
       });
 
       // Use query param assignment if possible
       if (assignmentMatchesName && isValidBucket) {
-        bucket = assignBucket;
+        this.bucket = assignBucket;
 
       // Select a random weighted bucket
       } else {
-        bucket = chooseWeightedItem(names, weights);
+        this.bucket = chooseWeightedItem(names, weights);
       }
 
       // Save
-      tests[name] = {
-        bucket,
-        buckets: Object.keys(data),
+      this.tests[this.name] = {
+        bucket: this.bucket,
+        buckets: Object.keys(this.data),
       };
-      storage.local.setItem(storageKey, JSON.stringify(tests));
+      storage.local.setItem(storageKey, JSON.stringify(this.tests));
     }
+  }
 
+  dispatchTest() {
     // We've now bucketed our user
     // Add classname
-    const className = `${name}--${bucket}`;
+    const className = `${this.name}--${this.bucket}`;
     if (supportsClasslist) {
       document.body.classList.add(className);
     } else {
       document.body.className += ` ${className}`;
     }
-
+    const { bucket } = this;
     const info = {
       bucket,
-      data: data[bucket],
+      data: this.data[bucket],
       active: true,
     };
 
     // Call function if provided
-    if (data[bucket]) {
-      if (!data[bucket].chosen) data[bucket].chosen = noop;
-      data[bucket].chosen.call(this);
+    if (this.data[bucket]) {
+      if (!this.data[bucket].chosen) this.data[bucket].chosen = noop;
+      this.data[bucket].chosen.call(this);
     }
 
     // Call chosen function
-    if (chosen) { chosen.call(this, info); }
+    if (this.chosen) { this.chosen.call(this, info); }
 
     // Record test with GTM if possible
     if (typeof dataLayer !== 'undefined') {
       const metrics = {
         abTests: {},
       };
-      metrics.abTests[name] = bucket;
+      metrics.abTests[this.name] = bucket;
       dataLayer.push(metrics);
     }
-
     // Return
     return info;
   }
